ee3d4a84f221dcef92301eb4126d25e6e3aa1964
josh <rainiser@icloud.com>
2023-03-05 16:44:07 -0800

    completed about josh

--- Milestones/M1/ThrowawayPrototype/throwaway/src/routes/josh/+page.svelte
+++ Milestones/M1/ThrowawayPrototype/throwaway/src/routes/josh/+page.svelte
@@ -3,3 +3,4 @@
     My name is Josh (he/him/his) and I am currently working on my first Bachelor's degree in CS.
     Some interests of mine include music, plants, and trying new foods. 
 </p> 
+<img src="../joshPic.jpeg" alt="Calathea Plant">
