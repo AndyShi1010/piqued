ba94a346b7d72485a03a8fa9adf6865f572720a4
ando <andy1010999@outlook.com>
2023-03-28 14:20:15 -0700

    Vertical Prototype. Created Button and NavBar.

---
+++ Milestones/M2/VerticalPrototype/vertPrototype/src/lib/Nav.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Button from "./Button.svelte";
+</script>
+
+<div id="nav">
+    <div id="logo">
+
+    </div>
+    <div id="action-buttons">
+        <Button to="/login" secondary>Log In</Button>
+        <Button to="/signup">Sign Up</Button>
+    </div>
+</div>
+
+<style>
+    div#nav {
+        border-bottom: 1px solid black;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        padding: 8px 0px;
+        background-color: #FCCFC5;
+    }
+    div#logo {
+        width: 120px;
+        margin-left: 32px;
+        border: 1px solid black;
+    }
+    div#action-buttons {
+        display: flex;
+        gap: 8px;
+        margin-right: 32px;
+    }
+</style>
