e78d402125bcf595eec44a943feae0d8e298ed4e
Leo Saeteurn <leosaeteurn@gmail.com>
2023-03-29 22:21:44 -0700

    added folder for backend and a java file for database

---
+++ Milestones/M2/VerticalPrototype/backend/Database.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Database {
+    public static void main(String[] args){
+        try{
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
