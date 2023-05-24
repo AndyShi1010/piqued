b3b03506e0388413aa1740c0cd01a54e5c3ef74e
Nishit <nishitpachchigar7117@gmail.com>
2023-05-24 08:31:11 -0700

    Updating the deploy branch

---
+++ Milestones/M4/BetaPrototype/Piqued/dist/assets/Biopage-d505f30c.js
@@ -0,0 +1,5 @@
+import{S as L,i as k,s as w,e as m,b as d,c as $,d as c,f as H,g as r,m as g,o as M,t as h,k as v,l as P,n as _}from"./index-9b1d5904.js";import"./Tag-69ee6958.js";import{C as b}from"./Card-c219b4e5.js";function S(x){let e,i,f,o,p,a,n,u,s,l;return n=new b({}),s=new b({}),{c(){e=m("div"),i=m("div"),i.innerHTML=`<img/> 
+        <h2>Username</h2> 
+        <h2>Full name</h2> 
+        <h2>Email</h2>`,f=d(),o=m("div"),o.innerHTML=`<a href="">User&#39;s Posts</a> 
+        <a href="">Liked Posts</a>`,p=d(),a=m("div"),$(n.$$.fragment),u=d(),$(s.$$.fragment),c(i,"class","main-account"),c(o,"class","post-bar"),c(a,"class","articles"),c(e,"class","biopage")},m(t,C){H(t,e,C),r(e,i),r(e,f),r(e,o),r(e,p),r(e,a),g(n,a,null),r(a,u),g(s,a,null),l=!0},p:M,i(t){l||(h(n.$$.fragment,t),h(s.$$.fragment,t),l=!0)},o(t){v(n.$$.fragment,t),v(s.$$.fragment,t),l=!1},d(t){t&&P(e),_(n),_(s)}}}class y extends L{constructor(e){super(),k(this,e,null,S,w,{})}}export{y as default};
